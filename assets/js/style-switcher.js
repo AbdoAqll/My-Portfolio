6e00299a3d1e63d789fb82360139b6c9559970f9
عبدالرحمن نصر عقل خليل <102249752+AbdoAqll@users.noreply.github.com>
2025-02-17 19:03:27 +0200

    update the default theme and default skin

--- assets/js/style-switcher.js
+++ assets/js/style-switcher.js
@@ -31,19 +31,17 @@ function setActiveStyle(color)
 
 /* theme light dark mode */
 const dayNight = document.querySelector(".day-night");
+
+// Set the default theme to dark on page load
+window.addEventListener("load", () => {
+    document.body.classList.add("dark");
+    dayNight.querySelector("i").classList.add("fa-sun");  // Set the sun icon for dark mode
+});
+
 dayNight.addEventListener("click", () => {
     dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
     document.body.classList.toggle("dark");
-})
-window.addEventListener("load", () => {
-    if(document.body.classList.contains("dark"))
-    {
-        dayNight.querySelector("i").classList.add("fa-sun");
-    }
-    else
-    {
-        dayNight.querySelector("i").classList.add("fa-moon");
-    }
-})
+});
+
 
